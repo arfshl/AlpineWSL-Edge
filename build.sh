a101e9891fb40b9e8c79d996d7be26b63371b105
arfshl <98870352+arfshl@users.noreply.github.com>
2026-04-21 18:12:44 +0700

    Project remake #1

---
+++ build.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+export RELEASE=20260127
+ARCH=$(uname -m)
+case "$ARCH" in
+    x86_64) ARCH=x86_64 ;;
+    amd64) ARCH=x86_64 ;;
+    aarch64) ARCH=aarch64 ;;
+    arm64) ARCH=aarch64 ;;
+    *)
+        echo "Unsupported architecture: $ARCH"
+        exit 1
+        ;;
+esac
+echo "RELEASE=$RELEASE" >> "$GITHUB_ENV"
+echo "ARCH=$ARCH" >> "$GITHUB_ENV"
+
+
+# start build
+curl -LO https://dl-cdn.alpinelinux.org/alpine/edge/releases/$ARCH/alpine-minirootfs-$RELEASE-$ARCH.tar.gz
+mkdir -p ./alpinewsl
+tar -xjpf alpine-minirootfs-$RELEASE-$ARCH.tar.gz -C ./alpinewsl
+sudo cp oobe.sh ./alpinewsl/etc/oobe.sh
+sudo cp wsl-distribution.conf ./alpinewsl/etc/wsl-distribution.conf
+mkdir -p ./alpinewsl/usr/lib/wsl/
+sudo curl -L https://raw.githubusercontent.com/yuk7/wsldl/refs/heads/main/res/Alpine/icon.ico --output-dir ./alpinewsl/usr/lib/wsl/
+
+cat <<-EOF | sudo unshare -mpf bash -e -
+sudo mount --bind /dev ./alpinewsl/dev
+sudo mount --bind /proc ./alpinewsl/proc
+sudo mount --bind /sys ./alpinewsl/sys
+sudo echo 'nameserver 1.1.1.1' >> ./alpinewsl/etc/resolv.conf
+
+sudo chroot ./alpinewsl apk update && apk upgrade
+sudo chroot ./alpinewsl apk add bash sudo
+EOF
+
+cd ./alpinewsl
+sudo tar --numeric-owner --absolute-names -c  * | gzip --best > ../install.tar.gz
+mv install.tar.gz alpine-wsl-edge.wsl
---
+++ oobe.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -ue
+
+DEFAULT_GROUPS='adm,cdrom,sudo,dip,plugdev'
+DEFAULT_UID='1000'
+
+echo 'Please create a default UNIX user account. The username does not need to match your Windows username.'
+echo 'For more information visit: https://aka.ms/wslusers'
+
+if getent passwd "$DEFAULT_UID" > /dev/null ; then
+  echo 'User account already exists, skipping creation'
+  exit 0
+fi
+
+while true; do
+
+  # Prompt from the username
+  read -p 'Enter new UNIX username: ' username
+
+  # Create the user
+  if /usr/sbin/adduser --uid "$DEFAULT_UID" --quiet --gecos ''  "$username"; then
+
+    if /usr/sbin/usermod "$username" -aG "$DEFAULT_GROUPS"; then
+      break
+    else
+      /usr/sbin/deluser "$username"
+    fi
+  fi
+done
+
+cat > /etc/sudoers.d/wsluser << EOF
+# Ensure the WSL initial user can use sudo without a password.	
+#
+# Since the user is in the wheel group, this file can be removed
+# if you wish to require a password for sudo. Be sure to set a
+# user password before doing so with 'sudo passwd $username'!
+$username ALL=(ALL) NOPASSWD: ALL
+EOF
+
+echo 'Your user has been created, is included in the wheel group, and can use sudo without a password.'	
+echo "To set a password for your user, run 'sudo passwd $username'"
