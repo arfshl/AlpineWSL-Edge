9157880e0093e0b3ff5647d25e8967fbee49ae4c
arfshl <98870352+arfshl@users.noreply.github.com>
2026-04-22 09:29:44 +0700

    refactor actions

--- build-edge.sh
+++ build-edge.sh
@@ -40,4 +40,4 @@ EOF
 
 cd ./alpinewsl
 sudo tar --numeric-owner --absolute-names -c  * | gzip --best > ../install.tar.gz
-mv ../install.tar.gz ../alpine-edge.wsl
+mv ../install.tar.gz ../alpine-edge-$ARCH.wsl
